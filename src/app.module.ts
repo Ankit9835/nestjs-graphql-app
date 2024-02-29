7617be918e376988eb93aaae02de5f346ea03e13
Ankit9835 <ankydchrismatic@gmail.com>
2024-02-29 23:54:50 +0530

    working on types and resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { LessonModule } from './lesson/lesson.module';
+
 
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    LessonModule
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
